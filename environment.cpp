237ce2edd3376576cbff85b73c48ed50d996719d
Niklas Samuelsson <niklas.samuelsson5@gmail.com>
2022-09-24 14:16:23 +0200

    All functionalities for a single agent enviroment implemented.

--- environment.cpp
+++ environment.cpp
@@ -3,6 +3,11 @@
 
 using namespace std;
 
+struct agentPosition{
+    int dim1;
+    int dim2;
+};
+
 struct transition{
     vector<vector<int>> oldState;
     int action;
@@ -11,26 +16,65 @@ struct transition{
     bool done;
 };
 
+/**
+ * Simulates a 2D room enviroment with an agent acting in it.
+ * The agent can perform one of five actions every timestep:
+ * move up, down, left, right or idle.
+ * A reward (cost) of 1 is given every timestep.
+ * The task is completed when every position has been visited.
+*/
 class Room{
     private:
         int m, n;
-        // 3D vector: row, col, map overlay
-        // Map overlay: cleaning status, agent location
+        agentPosition cAgentPos;
+        // 3D vector: row, col, map overlay[cleaning status, action location]
         vector<vector<vector<int>>> grid;
         vector<vector<int>> state;
 
-    bool CheckIfDone(){
-        bool done = true;
-        for(int i = 0; i < grid.size(); i++){
-            for(int j = 0; j < grid.at(i).size(); j++){
-                if(grid.at(i).at(j).at(0) == 0){
-                    done = false;
-                    break;
+        bool CheckIfDone(){
+            bool done = true;
+            for(int i = 0; i < grid.size(); i++){
+                for(int j = 0; j < grid.at(i).size(); j++){
+                    if(grid.at(i).at(j).at(0) == 0){
+                        done = false;
+                        break;
+                    }
                 }
             }
+            return done;
         }
-        return done;
-    }
+
+        int TakeAction(int action){
+            // 1: up (-1 in dim1)
+            // 2: down (+1 in dim1)
+            // 3: left (-1 in dim2)
+            // 4: right (+1 in dim2)
+            // 5: idle
+            int reward;
+
+            reward = 1;
+            grid.at(cAgentPos.dim1).at(cAgentPos.dim2).at(1) = 0;
+
+            switch (action){
+                case 1:
+                    cAgentPos.dim1 = (cAgentPos.dim1 - 1 < 0) ? cAgentPos.dim1: cAgentPos.dim1 - 1;
+                    break;
+                case 2:
+                    cAgentPos.dim1 = (cAgentPos.dim1 + 1 == m) ? cAgentPos.dim1: cAgentPos.dim1 + 1;
+                    break;
+                case 3:
+                    cAgentPos.dim2 = (cAgentPos.dim2 - 1 < 0) ? cAgentPos.dim2: cAgentPos.dim2 - 1;
+                    break;
+                case 4:
+                    cAgentPos.dim2 = (cAgentPos.dim2 + 1 == n) ? cAgentPos.dim2: cAgentPos.dim2 + 1;
+                    break;
+                case 5:
+                    break;
+            }
+            grid.at(cAgentPos.dim1).at(cAgentPos.dim2).at(0) = 1;
+            grid.at(cAgentPos.dim1).at(cAgentPos.dim2).at(1) = 2;
+            return reward;
+        }
 
     public:
         Room(int m, int n){
@@ -43,7 +87,11 @@ class Room{
             grid.clear();
             grid.assign(m, vector(n, vector<int>(2, 0)));
             // Assign agent start location
-            grid.at(0).at(0).at(1) = 1;
+            cAgentPos.dim1 = 0;
+            cAgentPos.dim2 = 0;
+            grid.at(cAgentPos.dim1).at(cAgentPos.dim2).at(1) = 2;
+            // Mark the first postion as cleaned
+            grid.at(cAgentPos.dim1).at(cAgentPos.dim2).at(0) = 1;
         }
 
         void Render(int layer){
@@ -79,11 +127,7 @@ class Room{
 
             newTransition.oldState = GetState();
             newTransition.action = action;
-
-            // TODO: implement reward function
-            // TakeAction()
-            //      GetReward()
-            newTransition.reward = 1;
+            newTransition.reward = TakeAction(action);
             newTransition.newState = GetState();
             newTransition.done = CheckIfDone();
 
@@ -93,15 +137,12 @@ class Room{
 
 int main(){
     Room TestRoom(6, 10);
-    TestRoom.Render(0);
-    cout<<"\n";
-    TestRoom.Render(1);
-    cout<<"\n";
-    vector<vector<int>> a = TestRoom.GetState();
-    for(int i = 0; i <= a.size()-1; i++){
-        for(int j = 0; j <= a.at(i).size()-1; j++){
-            cout<<a.at(i).at(j);
-        }
+    transition temp;
+    for(int i = 0; i < 10-1; i++){
+        temp = TestRoom.Step(4);
+        TestRoom.Render(0);
+        cout<<"\n";
+        TestRoom.Render(1);
         cout<<"\n";
     }
 }
